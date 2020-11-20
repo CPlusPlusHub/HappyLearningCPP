ec9ebdf7cf99ac044d12a78c1f2504fdf576553c
Zreal <zenglz_pro@163.com>
2020-11-20 19:29:15 +0800

    add common_data as a parents class for method class

---
+++ mnist_ml/include/common.hpp
@@ -0,0 +1,36 @@
+#ifndef __COMMON_HPP
+#define __COMMON_HPP
+#include "data.hpp"
+#include <vector>
+
+class common_data
+{
+private:
+    /* data */
+protected:
+    std::vector<data *> * training_data;
+    std::vector<data *> * test_data;
+    std::vector<data *> * validation_data;
+
+public:
+    common_data
+(/* args */);
+    ~common_data
+();
+    void set_training_data(std::vector<data *> * vect);
+    void set_test_data(std::vector<data *> * vect);
+    void set_validation_data(std::vector<data *> * vect);
+};
+
+common_data::common_data(/* args */)
+{
+
+}
+
+common_data::~common_data()
+{
+    
+}
+
+
+#endif
