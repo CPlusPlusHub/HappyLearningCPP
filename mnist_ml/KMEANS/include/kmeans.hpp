c3ccc7bb4f46f38b7cb105bc9fc81b15a95f1e4f
Zreal <zenglz_pro@163.com>
2020-11-24 15:20:03 +0800

    finish kmeans

--- mnist_ml/KMEANS/include/kmeans.hpp
+++ mnist_ml/KMEANS/include/kmeans.hpp
@@ -3,6 +3,7 @@
 
 #include "common.hpp"
 #include <unordered_set>
+#include <unordered_map>
 #include <limits>
 #include <cstdlib>
 #include <cmath>
@@ -11,17 +12,17 @@
 
 typedef struct cluster{
     std::vector<double> *centroid;
-    std::vector<data *> *cluster_points;
+    std::unordered_set<data *> * cluster_points;
     std::map<uint8_t,int> class_counts;
     int most_frequent_label;
 
     cluster(data *initial_point){
         centroid = new std::vector<double>;
-        cluster_points = new std::vector<data *>;
+        cluster_points = new std::unordered_set<data *>;
         for(auto value: *(initial_point->get_feature_vector())){
             centroid->push_back(value);
         }
-        cluster_points->push_back(initial_point);
+        cluster_points->insert(initial_point);
         class_counts[initial_point->get_label()] = 1;
         most_frequent_label = initial_point->get_label();
     }
@@ -31,14 +32,31 @@ typedef struct cluster{
             double tmp = centroid->at(i) * cluster_points->size();
             tmp += point->get_feature_vector()->at(i);
             tmp /= (double)(cluster_points->size() + 1);
+            centroid->at(i) = tmp;
         }
 
         if(class_counts.find(point->get_label()) == class_counts.end()){
-            class_counts[point->get_label()] == 1;
+            class_counts[point->get_label()] = 1;
         }else{
             class_counts[point->get_label()] += 1;
         }
-        cluster_points->push_back(point);
+        cluster_points->insert(point);
+        set_most_frequent_class();
+    }
+
+    void erase_from_cluster(data* point){
+        if(cluster_points->find(point) == cluster_points->end()){
+            printf("Error the point doesn\'t exit in this cluster\n");
+            return;
+        }
+        for(size_t i = 0; i < centroid->size(); i++){
+            double value = centroid->at(i) * cluster_points->size();
+            value -= point->get_feature_vector()->at(i);
+            value /= (double)(cluster_points->size() - 1);
+            centroid->at(i) = value;
+        }
+        class_counts[point->get_label()] -= 1;
+        cluster_points->erase(point);
         set_most_frequent_class();
     }
 
@@ -62,14 +80,17 @@ private:
     /* data */
     int num_clusters;
     std::vector<cluster_t *> *cluster_list;
+    std::unordered_map<int, int> index_map;
 public:
     kmeans(/* args */int);
+    kmeans();
     void init_clusters();
     void init_clusters_for_each_class();
-    void train(int fre = 10);
+    void train(int epoches = 10);
     double euclidean_distance(std::vector<double> *, data *);
     double validation();
     double test();
+    int predict(data *);
     ~kmeans();
 };
 
--- mnist_ml/KMEANS/src/kmeans.cc
+++ mnist_ml/KMEANS/src/kmeans.cc
@@ -4,9 +4,12 @@ kmeans::kmeans(int val){
     num_clusters = val;
     cluster_list = new std::vector<cluster_t *>;
 }
+kmeans::kmeans(){
+    cluster_list = new std::vector<cluster_t *>;
+}
 
 kmeans::~kmeans(){
-
+    free(cluster_list);
 }
 
 void kmeans::init_clusters(){
@@ -18,6 +21,7 @@ void kmeans::init_clusters(){
         }
         used_index.insert(index);
         cluster_t *c = new cluster(training_data->at(index));
+        index_map.insert(std::pair<int,int>(index,cluster_list->size()));
         cluster_list->push_back(c);
     }
     printf("Initialize clusters");
@@ -29,28 +33,42 @@ void kmeans::init_clusters_for_each_class(){
         int label = training_data->at(i)->get_label();
         if(used_class.find(label) == used_class.end()){
             used_class.insert(label);
+            index_map.insert(std::pair<int,int>(i,cluster_list->size()));
             cluster_list->push_back(new cluster(training_data->at(i)));
         }
     }
     printf("Initialize clusters for each class");
 }
 
-void kmeans::train(int epoches = 10){
+void kmeans::train(int epoches){
     for(size_t epoch = 0; epoch < epoches; epoch++){
-        for(auto point : *training_data){
+        for(size_t index = 0; index < training_data->size(); index++){
             int best_cluster_index = 0;
             double min_distance = std::numeric_limits<double>::max();
+            
             for(size_t i = 0; i < cluster_list->size(); i++){
-                double distance = euclidean_distance(cluster_list->at(i)->centroid, point);
+                double distance = euclidean_distance(cluster_list->at(i)->centroid, training_data->at(index));
                 if(distance < min_distance){
                     best_cluster_index = i;
-                    min_distance = distance
+                    min_distance = distance;
                 }
             }
+
+            if(index_map.find(index) == index_map.end()){
+                index_map[index] = best_cluster_index;
+            }
+            else if(index_map[index] != best_cluster_index){
+                cluster_list->at(index_map[index])->erase_from_cluster(training_data->at(index));
+                cluster_list->at(best_cluster_index)->erase_from_cluster(training_data->at(index));
+            }
+            
         }
     }
 }
+/* 
+    calculate the euclidean distance between centroid and query_point
 
+ */
 double kmeans::euclidean_distance(std::vector<double>* centroid, data * point){
     double distance = 0.0;
     for(size_t i = 0; i < centroid->size(); i++){
@@ -59,10 +77,67 @@ double kmeans::euclidean_distance(std::vector<double>* centroid, data * point){
     return sqrt(distance);
 }
 
-double kmeans::validation(){
+int kmeans::predict(data* query_point){
+    int best_cluster = 0;
+    double  min_distance = std::numeric_limits<double>::max();   
+    for(size_t i = 0; i < num_clusters; i++){
+       double distance = euclidean_distance(cluster_list->at(i)->centroid,query_point);
+       if(distance < min_distance){
+           best_cluster = i;
+           min_distance = distance;
+       }
+    }
+    return cluster_list->at(best_cluster)->most_frequent_label;
+}
 
+double kmeans::validation(){
+    double current_performance = 0.0;
+    int count = 0;
+    int index = 0;
+    for(auto query_point : *validation_data){
+        int pred_label = predict(query_point);
+        if(pred_label == query_point->get_label()){
+            count++;
+        }
+        index++;
+        current_performance = (double)count/(double)index * 100;
+        printf("Validation:%d:%lu Current Performance:%.3f%%\n|",index,validation_data->size(),current_performance);
+    }
+    printf("Validation Final Performance Accuracy %.3f%%",current_performance);
+    return current_performance;
 }
 
 double kmeans::test(){
+    double current_performance = 0.0;
+    int count = 0;
+    int index = 0;
+    for(auto query_point : *test_data){
+        int pred_label = predict(query_point);
+        if(pred_label == query_point->get_label()){
+            count++;
+        }
+        index++;
+        current_performance = (double)count/(double)index * 100;
+        printf("Test:%d:%lu Current Performance:%.3f%%\n|",index,test_data->size(),current_performance);
+    }
+    printf("Test Final Performance Accuracy %.3f%%",current_performance);
+    return current_performance;
+}
+
+int main(){
+    data_handler *dh = new data_handler();
+    dh->read_feature_vector("../mnist_data/t10k-images-idx3-ubyte");
+    dh->read_feature_labels("../mnist_data/t10k-labels-idx1-ubyte");
+    dh->split_data();
+    dh->count_classes(); 
+    kmeans * model = new kmeans(10);
+    model->set_test_data(dh->get_test_data());
+    model->set_training_data(dh->get_training_data());
+    model->set_validation_data(dh->get_validation_data());
+
+    model->init_clusters_for_each_class();
+    model->train();
+    model->validation();
+    model->test();
 
 }
